026b53b4bdd31c915f11efa90178381f270253d0
iwHiteRabbiT <50343833+iwHiteRabbiT@users.noreply.github.com>
2020-04-26 22:38:13 +0200

    Can deal with Megascans 3D Asset - Generate WorldSpace NormalMap with xNormal

--- MainForm.cs
+++ MainForm.cs
@@ -6,15 +6,19 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace TexChannelPacker
 {
     public partial class MainForm : Form
     {
+        public const string xNormal = @"C:\Softs\xNormal\3.19.3\x64\xNormal.exe";
+
         protected class BMP
         {
             public int Width;
@@ -53,13 +57,13 @@ namespace TexChannelPacker
 
             var filepath = GetFilePath("Albedo");
 
+            var meshfile = "";
+
             if(filepath.ToLower().EndsWith(".zip"))
             {
                 InputBox("Material Name", "Material Name:", ref name);
-                var dir = Path.GetDirectoryName(filepath) + @"\" + name;
-                newfilepath = dir;
-
-                dir += "TMP";
+                newfilepath = Path.GetDirectoryName(filepath);
+                var dir = newfilepath + @"\" + name + "TMP";
 
                 ZipFile.ExtractToDirectory(filepath, dir);
                 
@@ -71,19 +75,22 @@ namespace TexChannelPacker
                     bmp.Dispose();
 
                     filepath = FindSupportedImg(dir, "*_AO*");
-                    bmp = new Bitmap(filepath);
-                    bmpAO = GetData(bmp);
-                    bmp.Dispose();
+                    if (!String.IsNullOrEmpty(filepath))
+                    {
+                        bmp = new Bitmap(filepath);
+                        bmpAO = GetData(bmp);
+                        bmp.Dispose();
+                    }
 
                     filepath = FindSupportedImg(dir, "*_Cavity*");
                     bmp = new Bitmap(filepath);
                     bmpCav = GetData(bmp);
                     bmp.Dispose();
 
-                    filepath = FindSupportedImg(dir, "*_Specular*");
-                    bmp = new Bitmap(filepath);
-                    bmpSpec = GetData(bmp);
-                    bmp.Dispose();
+                    // filepath = FindSupportedImg(dir, "*_Specular*");
+                    // bmp = new Bitmap(filepath);
+                    // bmpSpec = GetData(bmp);
+                    // bmp.Dispose();
 
                     filepath = FindSupportedImg(dir, "*_Normal*");
                     bmp = new Bitmap(filepath);
@@ -99,6 +106,27 @@ namespace TexChannelPacker
                     bmp = new Bitmap(filepath);
                     bmpRough = GetData(bmp);
                     bmp.Dispose();
+
+                    var fbxes = Directory.GetFiles(dir, "*").Where(x => x.ToLower().EndsWith("fbx")).ToList();
+                    if (fbxes.Count > 0)
+                    {
+                        newfilepath += @"\" + name + @"\";
+                        Directory.CreateDirectory(newfilepath);
+
+                        foreach(var fp in fbxes)
+                        {
+                            var fn = Path.GetFileName(fp);
+                            fn = Regex.Replace(fn, "(.+)(_LOD.+)", name + "$2");
+                            File.Copy(fp, newfilepath + fn);
+
+                            if (Regex.IsMatch(fn, ".+_LOD0.+"))
+                                meshfile = newfilepath + fn;
+                        }
+                    }
+                    else
+                        newfilepath += @"\2K\";
+
+                    newfilepath += name;
                 }
                 finally
                 {
@@ -131,6 +159,8 @@ namespace TexChannelPacker
                 newfilepath = Path.GetDirectoryName(filepath) + @"\" + name;
             }
 
+            var resize = !String.IsNullOrEmpty(meshfile);
+
             var nC = 3;
             var w = bmpAlbedo.Width;
             var h = bmpAlbedo.Height;
@@ -144,19 +174,43 @@ namespace TexChannelPacker
                     var p = x * nC + y * s;
 
                     var Albedo = GetPixel(x, y, bmpAlbedo);
-                    var AO = GetPixel(x, y, bmpAO);
-                    // var Spec = GetPixel(x, y, bmpSpec);
 
-                    var fAO = AO[0]/255.0f;
-                    // var fSpec = Spec[0]/255.0f;
-                    // fSpec *= fSpec * fAO * fAO;
+                    var fAO = 1.0f;
+
+                    if (bmpAO != null)
+                    {
+                        var AO = GetPixel(x, y, bmpAO);
+                        // var Spec = GetPixel(x, y, bmpSpec);
+
+                        fAO = AO[0]/255.0f;
+                        // var fSpec = Spec[0]/255.0f;
+                        // fSpec *= fSpec * fAO * fAO;
+                    }
 
                     rgb[p + 2] = System.Convert.ToByte(Albedo[0] * fAO);
                     rgb[p + 1] = System.Convert.ToByte(Albedo[1] * fAO);
                     rgb[p + 0] = System.Convert.ToByte(Albedo[2] * fAO);
                 }                
 
-            WriteData(newfilepath + "_AAO.png", rgb, w, h);
+            WriteData(newfilepath + "_AAO.png", rgb, w, h, 2048, resize);
+
+            // (RGB)Norm for xNormal
+            for (var x=0 ; x<w ; x++)
+                for (var y=0 ; y<h ; y++)
+                {
+                    var p = x * nC + y * s;
+
+                    var Norm = GetPixel(x, y, bmpNorm);
+
+                    rgb[p + 2] = Norm[0];
+                    rgb[p + 1] = Norm[1];
+                    rgb[p + 0] = Norm[2];
+                }                
+
+            WriteData(newfilepath + "_N.png", rgb, w, h, 2048, resize);
+
+            if (!String.IsNullOrEmpty(meshfile))
+                xNormalConverter(meshfile, newfilepath + "_N.png", newfilepath + "_WSN.png");
 
             // (RGB)Norm
             for (var x=0 ; x<w ; x++)
@@ -171,7 +225,7 @@ namespace TexChannelPacker
                     rgb[p + 0] = Norm[2];
                 }                
 
-            WriteData(newfilepath + "_N.png", rgb, w, h);
+            WriteData(newfilepath + "_N.png", rgb, w, h, 2048, resize);
 
             // (R)Displ ; (G)Rough ; (B)Cavity
             for (var x=0 ; x<w ; x++)
@@ -188,19 +242,19 @@ namespace TexChannelPacker
                     rgb[p + 0] = Cav[0];
                 }                
 
-            WriteData(newfilepath + "_DRC.png", rgb, w, h);
+            WriteData(newfilepath + "_DRC.png", rgb, w, h, 1024, resize);
         }
 
-        protected string FindSupportedImg(string dir, string search)
+        protected static string FindSupportedImg(string dir, string search)
         {
             return Directory.GetFiles(dir, search).Where(x =>
                 x.ToLower().EndsWith("png") ||
                 x.ToLower().EndsWith("bmp") ||
                 x.ToLower().EndsWith("jpg") ||
-                x.ToLower().EndsWith("gif")).First();
+                x.ToLower().EndsWith("gif")).FirstOrDefault();
         }
 
-        public string GetFilePath(string title)
+        public static string GetFilePath(string title)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
@@ -219,7 +273,7 @@ namespace TexChannelPacker
             return null;
         }
 
-        protected BMP GetData(Bitmap bmp)
+        protected static BMP GetData(Bitmap bmp)
         {
             var newBmp = new BMP();
             newBmp.Width = bmp.Width;
@@ -250,7 +304,7 @@ namespace TexChannelPacker
             return newBmp;
         }
 
-        protected void WriteData(string filepath, byte[] rgbValues, int width, int height)
+        protected static void WriteData(string filepath, byte[] rgbValues, int width, int height, int newSize = 0, bool resize = false)
         {
             // Create a new bitmap.
             Bitmap bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
@@ -273,10 +327,17 @@ namespace TexChannelPacker
             // Unlock the bits.
             bmp.UnlockBits(bmpData);
 
-            bmp.Save(filepath, System.Drawing.Imaging.ImageFormat.Png);
+            if (resize)
+            {
+                var finalImg = ResizeImage(bmp, newSize);
+
+                finalImg.Save(filepath, System.Drawing.Imaging.ImageFormat.Png);
+            }
+            else
+                bmp.Save(filepath, System.Drawing.Imaging.ImageFormat.Png);
         }
 
-        protected byte[] GetPixel(int x, int y, BMP bmp)
+        protected static byte[] GetPixel(int x, int y, BMP bmp)
         {
             var pixel = new byte[] { 0, 0, 0, 0 };
 
@@ -355,6 +416,69 @@ namespace TexChannelPacker
             DialogResult dialogResult = form.ShowDialog();
             value = textBox.Text;
             return dialogResult;
+        }
+
+        public static Image ResizeImage(Image image, int size, bool preserveAspectRatio = false)
+        {
+            int newWidth;
+            int newHeight;
+            if (preserveAspectRatio)
+            {
+                int originalWidth = image.Width;
+                int originalHeight = image.Height;
+                float percentWidth = (float)size / (float)originalWidth;
+                float percentHeight = (float)size / (float)originalHeight;
+                float percent = percentHeight < percentWidth ? percentHeight : percentWidth;
+                newWidth = (int)(originalWidth * percent);
+                newHeight = (int)(originalHeight * percent);
+            }
+            else
+            {
+                newWidth = size;
+                newHeight = size;
+            }
+
+            Image newImage = new Bitmap(newWidth, newHeight);
+            using (Graphics graphicsHandle = Graphics.FromImage(newImage))
+            {
+                graphicsHandle.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphicsHandle.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+            return newImage;
+        }
+
+        public static void xNormalConverter(string objpath, string npath, string wsnpath)
+        {
+            var param = string.Format("-os2ts {0} true {1} {2} false 16", objpath, npath, wsnpath);
+
+            var proc = System.Diagnostics.Process.Start(xNormal, param);
+            proc.WaitForExit();
+            proc.Close();
+
+            var bmp = new Bitmap(wsnpath);
+            var bmpWSN = GetData(bmp);
+            bmp.Dispose();
+
+            // (RGB)Norm
+            var nC = 3;
+            var w = bmpWSN.Width;
+            var h = bmpWSN.Height;
+            var rgb = new byte[w * h * nC];
+            var s = w * nC;
+
+            for (var x=0 ; x<w ; x++)
+                for (var y=0 ; y<h ; y++)
+                {
+                    var p = x * nC + y * s;
+
+                    var Norm = GetPixel(x, y, bmpWSN);
+
+                    rgb[p + 2] = Norm[0];
+                    rgb[p + 1] = Norm[2];//System.Convert.ToByte(0xFF - Norm[1]);
+                    rgb[p + 0] = Norm[1];
+                }                
+
+            WriteData(wsnpath, rgb, w, h);
         }
     }
 }
